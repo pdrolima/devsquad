b28a58f45bd20c3684602b6253aa9d72686e99bc
webmasterdro <pedro-octavio@hotmail.com>
2019-08-15 00:15:22 -0300

    feat: cooking frontend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\User;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
+        view()->composer('layouts.app', function ($view) {
+            $view->with('extra', User::appVars());
+        });
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function appVars()
+    {
+        return [
+            'user' => auth()->user() ? auth()->user()->only('id', 'name', 'email', 'api_token') : []
+        ];
+    }
 }
